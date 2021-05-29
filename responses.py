b9f9056a249cfbf931026b3a5aa5c1823fdc6859
Mr.Nobody <mrnobody@pop-os.localdomain>
2021-05-29 17:21:03 +0530

    updated response

--- responses.py
+++ responses.py
@@ -1,6 +1,7 @@
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
 import subprocess
+from pythonping import ping
 
 def sitestatus_responses(input_text):
     user_message = str("https://"+input_text).lower()
@@ -17,8 +18,8 @@ def sitestatus_responses(input_text):
 
 def ipstatus_chceck(input_text):
     address = input_text
-    responseIP = subprocess.call(['ping -c 3', address])
-
+    #responseIP = subprocess.call(['ping -c 3', address])
+    responseIP = ping(address, size=40, count=3)
     if responseIP == 0:
         return "The Server Is UP And Responding 🥳"
     elif responseIP == 2:
